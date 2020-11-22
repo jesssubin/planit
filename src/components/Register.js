a56189a4c458418763547bad31762fc7513e1176
Jacqueline Johnstone-Gruba <jacqueline.mjg@hotmail.com>
2020-11-22 18:12:31 +0000

    Scss start

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,7 +3,11 @@ import React from "react";
 
 export default function Register (props) {
   return (
-    <div>
+    <div class="register">
+      <link rel="stylesheet" href="/styles/register.css" type="text/css" />
+     <div class="logo" id="logo">
+      <img src="./public/Planit.png" alt="avatar" class='avatar' />
+    </div>
     <form action="/register" method="POST">
       <h1>Create New Account</h1>
       <p>Please fill in this form</p>

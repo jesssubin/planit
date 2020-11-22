f577b893dc5f0d8f1f80d7452de2c9d9a2764317
Jacqueline Johnstone-Gruba <jacqueline.mjg@hotmail.com>
2020-11-22 20:26:23 +0000

    Add css for profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,17 +1,29 @@
 import React from "react";
+import "../profile.css"
+import logo from '../logo.png';
 
 
 export default function Profile (props) {
   return (
-    <div>
-      <h1>Planit</h1>
-      <div>
-      <p>Name: JJJ</p>
-      <p>Email: jjj@triplej.com</p>
+    <body>
+    <div class="profile">
+      <div class="profile">
+      <link href="'https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap'" rel="stylesheet"></link>
+      </div>
+      <div class="logo">
+      <img src={logo} alt="Logo" />
       </div>
       <div>
-      <button type="submit" id="logout" class="btn">Logout</button>
+        <h1>Profile</h1>
+      </div>
+      <div class="profile-details">
+        <p>Name: JJJ</p>
+        <p>Email: jjj@triplej.com</p>
+      </div>
+      <div class="logout-button">
+      <button type="submit" id="logout" class="logout-btn">Logout</button>
       </div>
     </div>
+    </body>
   );
 }

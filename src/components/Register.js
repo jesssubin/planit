25fd4e6e50f0877e89060826c6b5a92eb538c55c
jennyg0 <jen2348@hotmail.com>
2020-11-22 20:10:36 +0000

    add register page css

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,30 +1,38 @@
 import React from "react";
 import "../register.css"
+import logo from '../logo.png';
 
 export default function Register (props) {
   return (
     <div class="register">
-      <link href="'https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap'" rel="stylesheet"></link>
-     <div class="logo" id="logo">
-      <img src="../public/Planit.png" class='planitLogo' />
-    </div>
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet"></link>
+      <div class="registerLogo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div class="registerHeader">
+      <h1>Create a New Account</h1>
+      <p>Please fill in the form</p>
+      </div>
     <form action="/register" method="POST">
-      <h1>Create New Account</h1>
-      <p>Please fill in this form</p>
       <span>
-      <label for="name"></label>
-      <input type="name" placeholder="Enter Name" name="name" required />
+        <label for="name"></label>
+        <input type="name" placeholder="Enter Name" name="name" required />
       </span>
       <span>
-      <label for="email"></label>
-      <input type="email" placeholder="Enter Email" name="email" required />
+        <label for="email"></label>
+        <input type="email" placeholder="Enter Email" name="email" required />
       </span>
-      <label for="password"></label>
-      <input type="password" placeholder="Enter Password" name="password" required />
-      <button type="submit" class="btn btn-primary">Register</button>
-    </form>
-    <a href="/login" id='register-login'>Have an account? Login in here!</a>
-
+      <span>
+        <label for="password"></label>
+        <input type="password" placeholder="Enter Password" name="password" required />
+      </span>
+      <span>
+        <button type="submit" class="btn btn-primary">Register</button>
+      </span>
+      <div>
+        <span>Have an account? <a href="/login" id='register-login'>Sign in.</a></span>
+      </div>
+    </form>  
   </div>
   )
 }

72b2263b8fdf13e239f570142abf3988c0892435
Subin <jess.s.moon@gmail.com>
2020-11-22 21:34:41 -0500

    Add nav bar css -- completed

--- src/App.js
+++ src/App.js
@@ -17,9 +17,10 @@ function App() {
   return (
     <Router>
       <div>
+       <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
         <nav class="nav-bar">
-            <button onClick={<Explore />}><i class="fa fa-search"></i></button>
-            <button onClick={<Favourites />}><i class="fa fa-heart"></i></button>
+            <button onClick={<Explore />}><i class="fas fa-search"></i></button>
+            <button onClick={<Favourites />}><i class="far fa-heart"></i></button>
             <button onClick={<PlanList />}><i class="fa fa-calendar-plus"></i></button>
             <button onClick={<History />}><i class="fa fa-history"></i></button>
             <button onClick={<Profile />}><i class="fa fa-user"></i></button>

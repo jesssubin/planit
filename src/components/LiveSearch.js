6cb69090421de9616e40f94b94bd1969657d26c4
Jacqueline <jacqueline.mjg@hotmail.com>
2020-11-23 21:12:29 -0500

    First look at api search feature

--- src/components/LiveSearch.js
+++ src/components/LiveSearch.js
@@ -6,15 +6,12 @@ import ExploreResult from "./ExploreResult";
 const axios = require('axios');
 
 export default function LiveSearch(props) {
-  const [term, setTerm] = useState("squidward");
+  const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
 
   useEffect(() => {
-    // const testURL = `https://itunes.apple.com/search?term=${term}&country=CA&media=music&entity=album&attribute=artistTerm`
-    // ; 
-    const searchURL = `https://test.api.amadeus.com/v1/shopping/activities?latitude=41.397158&longitude=2.160873&radius=1`
 
-    axios.get(searchURL).then(response => {
+    axios.get('/search').then(response => {
       console.log(response);
       setResults([...response.data.results])
     });

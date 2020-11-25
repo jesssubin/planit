42e61dc77485c8d40ea26d5c92d8d0effb8b22e9
Subin <jess.s.moon@gmail.com>
2020-11-25 17:34:34 -0500

    Implementing activity details -- in progress

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import axios from 'axios'; 
+import ActivityDetails from './ActivityDetails'; 
 
 export default function Activity (props) {
-  
+  const [details, setDetails] = useState(false)
   let photoURL = ''
   
   if (props.photos){
@@ -36,22 +37,32 @@ export default function Activity (props) {
           });    
   }
   
+
   const onClick = () => {
     saveActivity();
     //console.log(props);
   }
+
+  const showDetails = () => {
+    // e.preventDefault();
+    return (
+        <ActivityDetails {...props} toggleDisplay={() => setDetails(false)}/>
+
+    )
+  }
   
   return (
     <article>
+      { details ? showDetails() :
       <div class="w3-card-4">
-      <img src={photoURL} alt="restaurant"/>
-      <header class="w3-container w3-light-grey">{props.name}</header>
-      <div class="w3-container">
-      <h5>{props.rating}</h5>
-      <p>Description goes here </p>
-    </div>
-    <button onClick={onClick} class="w3-button w3-block w3-dark-grey">Add to Favourites</button>
-    </div>
+        <img src={photoURL} alt="restaurant" />
+        <header class="w3-container w3-light-grey">{props.name} </header>
+        <div class="w3-container">
+          <h5>{props.rating}</h5>
+        </div>
+        <button onClick={() => setDetails(true)} class="w3-button w3-block w3-dark-grey">See Details </button>
+        <button onClick={onClick} class="w3-button w3-block w3-dark-grey">Add to Favourites </button>
+      </div>}
     </article>
   )
 }
